de2baff0f9c7ddc7023eda79337ed937b7eecf16
auazze <auazze.music@gmail.com>
2025-05-04 21:10:59 +0500

    hot-reload problem

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import FullReload from 'vite-plugin-full-reload'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-        FullReload([
-            'resources/views/**/*.blade.php',
-            'routes/**/*.php',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
     server: {
         host: '0.0.0.0',
         port: 5173,
-        hmr: { host: 'localhost' },
+        hmr: {
+            host: 'localhost',
+        },
     },
-})
+});
